1015ebf02af481a27ac1851cf789022ae56c50f2
t <t@tbarron.xyz>
2018-10-02 00:34:33 -0600

    adding functions for catenary degree and maxnondreducible elements for arbitrary distance functions

--- NumericalMonoid.ts
+++ NumericalMonoid.ts
@@ -1,5 +1,5 @@
-import { JSONHashSet } from './Hash';
-import { zeroThroughN, addOneAtIndex, tupleMinus, tupleGCD, sum } from './utils';
+import { JSONHashSet, HashSet } from './Hash';
+import { zeroThroughN, addOneAtIndex, tupleMinus, tupleGCD, sum, euclideanDistance } from './utils';
 
 declare var cytoscape;
 
@@ -58,7 +58,7 @@ export class NumericalMonoid {
             factorizations.forEach(fac1 => {
                 factorizations.forEach(fac2 => {
                     if (JSON.stringify(fac2) == JSON.stringify(fac1)) return;
-                    const distance = this.distance(fac1, fac2);
+                    const distance = this.distanceForClassicCatenary(fac1, fac2);
                     if (distance <= i) {
                         g.add({ data: { source: JSON.stringify(fac1), target: JSON.stringify(fac2) } });
                     }
@@ -71,10 +71,10 @@ export class NumericalMonoid {
         }
     }
 
-    maxnonreducibleEdges(n: number): number[][][] {
+    maxnonreducibleEdgesByMetric(n: number, metric: (fac1, fac2: number[]) => number): number[][][] {
         // Returns the set of nonreducible edges that are maximal (i.e. d = catenary degree)
         const factorizations = this.factorizations(n);
-        const catenaryDegree = this.catenaryDegree(n);
+        const catenaryDegree = this.catenaryDegreeByMetric(n, metric);
         const vertices = factorizations.map(fac => ({ data: { id: JSON.stringify(fac) } }));
         const g = cytoscape({
             elements: vertices
@@ -83,7 +83,7 @@ export class NumericalMonoid {
         for (let fac1 of factorizations) {
             for (let fac2 of factorizations) {
                 if (JSON.stringify(fac2) == JSON.stringify(fac1)) continue;
-                const d = this.distance(fac1, fac2);
+                const d = metric(fac1, fac2);
                 if (d < catenaryDegree) {
                     g.add({
                         data: {
@@ -112,7 +112,77 @@ export class NumericalMonoid {
         return returnSet.toArray();
     }
 
-    distance(fac1: number[], fac2: number[]): number {
+    catenaryDegreeByMetric(n: number, metric: (fac1, fac2: number[]) => number): number {
+        const factorizations = this.factorizations(n);
+        const pairwiseDistances = new Set<number>();
+        factorizations.forEach(fac1 => {
+            factorizations.forEach(fac2 => {
+                pairwiseDistances.add(euclideanDistance(fac1, fac2));
+            });
+        });
+        for (let pairDistance of pairwiseDistances) {
+            const g = cytoscape({});
+            factorizations.forEach(fac => g.add({ data: { id: JSON.stringify(fac) } }));    // first pass: add all nodes. can't add edges if one end isn't there yet
+            factorizations.forEach(fac1 => {
+                factorizations.forEach(fac2 => {
+                    if (JSON.stringify(fac2) == JSON.stringify(fac1)) return;
+                    const distance = metric(fac1, fac2);
+                    if (distance <= pairDistance) {
+                        g.add({ data: { source: JSON.stringify(fac1), target: JSON.stringify(fac2) } });
+                    }
+                })
+            });
+            if (g.elements().components().length == 1) {
+                this.cachedCatenaryDegrees.set(n, pairDistance);
+                return pairDistance;
+            }
+        }
+    }
+
+    catenaryDegreeEuclidean = (x: number) => this.catenaryDegreeByMetric(x, euclideanDistance)
+
+    maxnonreducibleEdges(n: number): number[][][] {
+        // Returns the set of nonreducible edges that are maximal (i.e. d = catenary degree)
+        const factorizations = this.factorizations(n);
+        const catenaryDegree = this.catenaryDegree(n);
+        const vertices = factorizations.map(fac => ({ data: { id: JSON.stringify(fac) } }));
+        const g = cytoscape({
+            elements: vertices
+        }); // just vertices for now, edges next
+        const maximalEdges = [];
+        for (let fac1 of factorizations) {
+            for (let fac2 of factorizations) {
+                if (JSON.stringify(fac2) == JSON.stringify(fac1)) continue;
+                const d = this.distanceForClassicCatenary(fac1, fac2);
+                if (d < catenaryDegree) {
+                    g.add({
+                        data: {
+                            source: JSON.stringify(fac1),
+                            target: JSON.stringify(fac2)
+                        }
+                    });
+                } else if (d == catenaryDegree) {
+                    maximalEdges.push([fac1, fac2]);
+                }
+            }
+        }
+
+        const returnSet = new JSONHashSet<number[][]>();
+        for (let [fac1, fac2] of maximalEdges) {
+            // If this maximal edge is not reducible...
+            if (!returnSet.has([fac2, fac1]) &&   // Make sure we don't have any reverse-order duplicates
+            !g.elements().aStar({
+                root: g.nodes(`node[id="${JSON.stringify(fac1)}"]`),
+                goal: g.nodes(`node[id="${JSON.stringify(fac2)}"]`)
+            }).found) {
+                returnSet.add([fac1, fac2]);
+            }
+        }
+
+        return returnSet.toArray();
+    }
+
+    distanceForClassicCatenary(fac1: number[], fac2: number[]): number {
         const gcd = tupleGCD(fac1, fac2);
         return Math.max(sum(tupleMinus(fac1, gcd)), sum(tupleMinus(fac2, gcd)));
     }
--- utils.ts
+++ utils.ts
@@ -9,3 +9,5 @@ export const addOneAtIndex = (array: number[], index: number) => {
 export const tupleGCD = (fac1: number[], fac2: number[]) => zeroThroughN(fac1.length).map(x => Math.min(fac1[x], fac2[x]));
 export const tupleMinus = (fac1: number[], fac2: number[]) => zeroThroughN(fac1.length).map(x => fac1[x] - fac2[x]);
 export const sum = (fac: number[]) => fac.reduce((a,b) => a+b, 0);
+
+export const euclideanDistance = (fac1: number[], fac2: number[]) => Math.pow(sum(fac1.map((el,i) => Math.pow(fac2[i] - fac1[i], 2))), 0.5);
